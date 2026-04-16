5b94de3636dcbd889230b17b74b875e610b87db8
Simon Cocking <simon@amplified.ai>
2026-04-16 17:27:27 +1000

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Amplified.OpenAlex.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @source_url "https://github.com/amplifiedai/amplified_openalex"
 
   def project do
